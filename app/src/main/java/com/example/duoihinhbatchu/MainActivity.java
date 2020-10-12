f9f1dd68318a06d765cba7aff34d52ccd4cd73f2
boyapdao <72749808+boyapdao@users.noreply.github.com>
2020-10-12 23:00:44 +0700

    Test ý mà

--- app/src/main/java/com/example/duoihinhbatchu/MainActivity.java
+++ app/src/main/java/com/example/duoihinhbatchu/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.duoihinhbatchu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn_Choi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Init();
+    }
+
+    private void Init() {
+        btn_Choi=findViewById(R.id.btn_Choi);
     }
 }
