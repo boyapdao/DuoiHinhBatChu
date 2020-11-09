e6538afefa9ce091067d8677b1460b613cbc8d35
boyapdao <72749808+boyapdao@users.noreply.github.com>
2020-11-09 19:22:58 +0700

    Thiết kế giao diện

--- app/src/main/java/com/example/duoihinhbatchu/MainActivity.java
+++ app/src/main/java/com/example/duoihinhbatchu/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.duoihinhbatchu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.duoihinhbatchu.api.GetCauHoi;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,8 +17,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new GetCauHoi().execute();
 
         Init();
+        Events();
+    }
+
+    private void Events() {
+        btn_Choi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (DATA.getData().arrCauDo.size()>0){
+                    Intent it=new Intent(MainActivity.this, ChoiGameActivity.class);
+                    startActivity(it);
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Rớt mạng rồi Đạik", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     private void Init() {
